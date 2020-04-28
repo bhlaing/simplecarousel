d0a3e77e6dbde13ec73e62888558773923260ac8
Billy <billhlaing@gmail.com>
2020-04-28 21:55:29 +1000

    Updated directories and packages

--- app/src/androidTest/java/com/simple/carousel/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/simple/carousel/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.dood.bhcarousel
+package com.simple.carousel
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.dood.bhcarousel", appContext.packageName)
+        assertEquals("com.simple.carousel", appContext.packageName)
     }
 }
--- app/src/test/java/com/simple/carousel/ExampleUnitTest.kt
+++ app/src/test/java/com/simple/carousel/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.dood.bhcarousel
+package com.simple.carousel
 
 import org.junit.Test
 
